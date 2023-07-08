f79f1b20e3a718bb1d80ddc303dd2b90cafa377a
Irwan Setiawan <irwan@irwansetiawan.com>
2023-07-09 00:45:52 +0800

    Add class structures

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,34 @@ import { ImprovMXConfig } from './improvmxconfig';
 
 export class ImprovMX {
     readonly baseUrl = 'https://api.improvmx.com/v3/';
+    config: ImprovMXConfig;
 
-    constructor(config: ImprovMXConfig) {}
+    constructor(config: ImprovMXConfig) {
+        this.config = config;
+    }
+
+    getAccount() {}
+    getAccountWhitelabels() {}
+
+    getDomains() {}
+    addDomain() {}
+    getDomain() {}
+    updateDomain() {}
+    deleteDomain() {}
+    isDomainMXEntriesValid() {}
+
+    getAliasesForDomain() {}
+    addAliasForDomain() {}
+    addAliasesForDomain() {}
+    getAliasForDomain() {}
+    updateAliasForDomain() {}
+    deleteAliasForDomain() {}
+
+    getLogsForDomain() {}
+    getLogsForAliasAndDomain() {}
+
+    getSMTPCredentials() {}
+    addSMTPCredential() {}
+    updateSMTPCredential() {}
+    deleteSMTPCredential() {}
 }
